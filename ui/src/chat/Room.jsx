bf0efada14f25794b74dbe708120c490ad37c430
Zach Auclair <zach101@gmail.com>
2017-09-16 16:28:29 -0400

    feat(ui/chat): display unread message indicator
    This also generally enhances scrolling logic (auto-advance on start),
    and adds fontawesome icons via cdn.

--- ui/src/chat/Room.jsx
+++ ui/src/chat/Room.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import moment from 'moment';
+import FontAwesome from 'react-fontawesome';
 import Message from './Message';
 import Api, { ApiListener } from '../lib/api';
 import { getScroll } from '../lib/document';
@@ -22,6 +23,8 @@ export default class Room extends Component {
         channels: {},
         emoji: {},
       },
+      unread: null,
+      startTs: moment(),
       messageTs: '',
       messages: [],
     };
@@ -29,8 +32,10 @@ export default class Room extends Component {
     this.handleEnter = this.handleEnter.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.pushOutboundMessage = this.pushOutboundMessage.bind(this);
+    this.viewUnreadMessages = this.viewUnreadMessages.bind(this);
     this.handleMessage = this.handleMessage.bind(this);
     this.handleConnectionStateChange = this.handleConnectionStateChange.bind(this);
+    window.onscroll = this.onScroll.bind(this);
 
     Api.register(new (class RoomListener extends ApiListener {
       onMessage = msg => this.handleMessage(msg);
@@ -43,17 +48,17 @@ export default class Room extends Component {
     const { messageTs } = this.state;
     // if we didn't append a message to our list, don't scroll
     if (messageTs === prevMessageTs) {
-      // console.log('[room] state changed but messageTs is the same');
+      // console.log('[room.component-did-update] state changed but messageTs is the same');
       return;
     }
     const [, y] = getScroll();
     // if user is 85% or greater scrolled to the bottom, give them the new message...
     const scrollPerc = (100 / document.body.scrollHeight) * y;
     if (scrollPerc >= 85) {
-      // console.log(`[room] scrolling to bottom (scrolled ${scrollPerc})`);
+      // console.log(`[room.component-did-update] scrolling to bottom (scrolled ${scrollPerc})`);
       window.scrollTo(0, document.body.scrollHeight);
     } else {
-      // console.log(`[room] skipping scroll ${scrollPerc}`);
+      // console.log(`[room.component-did-update] skipping scroll ${scrollPerc}`);
     }
   }
 
@@ -81,6 +86,24 @@ export default class Room extends Component {
     this.setState({ outboundMessage: '' });
   }
 
+  onScroll() {
+    const [, y] = getScroll();
+    // if user is 85% or greater scrolled to the bottom, mark read.
+    const scrollPerc = (100 / document.body.scrollHeight) * y;
+    if (scrollPerc < 85) {
+      return;
+    }
+    const { unread } = this.state;
+    if (!unread) {
+      return;
+    }
+    this.setState({ unread: null });
+  }
+
+  viewUnreadMessages() {
+    window.scrollTo(0, document.body.scrollHeight);
+  }
+
   handleMessage(msg) {
     switch (msg.type) {
       case 'team-info': {
@@ -105,37 +128,64 @@ export default class Room extends Component {
         break;
       }
       case 'message': {
-        const { messages } = this.state;
+        const { messages, unread, startTs } = this.state;
         // TODO edits, emoji, deletes, sorting, etc
 
         // we got an invalid or old message, drop it.
         if (msg.ts == null) {
           // eslint-disable-next-line no-console
-          console.log('Dropping invalid message', msg);
+          // console.log('[room.handle-message] dropping invalid message', msg);
           return;
         } else if (messages.length !== 0 && +(messages[messages.length - 1].ts) > +msg.ts) {
           // eslint-disable-next-line no-console
-          console.log('Dropping old message', msg);
+          // console.log('[room.handle-message] dropping old message', msg);
           return;
         }
         messages.push(msg);
+        let newUnreads = unread;
+        // if we got a message after our initial payload, it can be 'unread.'
+        // make sure that it's not a duplicate.
+        // if we're not scrolled to the bottom, append to unreads.
+        // once we scroll to the bottom or click the message, mark as read.
+        // otherwise, clear unreads.
+        const parsedMessageTs = moment(msg.ts * 1000);
+        if (startTs < parsedMessageTs && (!unread || unread.since < parsedMessageTs)) {
+          if (unread) {
+            newUnreads.count += 1;
+          } else {
+            newUnreads = { count: 1, since: parsedMessageTs };
+          }
+          // eslint-disable-next-line no-console
+          // console.log('[room.handle-message] adding unread message', msg, ', new unreads', newUnreads);
+        } else {
+          // eslint-disable-next-line no-console
+          // console.log('[room.handle-message] adding normal message', msg);
+
+          // if we're still backfilling messages, auto-advance us
+          // eslint-disable-next-line no-lonely-if
+          if (parsedMessageTs < startTs) {
+            window.scrollTo(0, document.body.scrollHeight);
+          }
+        }
+
         this.setState({
           messageTs: msg.ts,
           messages,
+          unread: newUnreads,
         });
         break;
       }
       default: {
         // eslint-disable-next-line no-console
-        console.log('Unhandled message', msg);
+        console.warn('[room.handle-message] unhandled message', msg);
         break;
       }
     }
   }
 
   render() {
-    const { handleChange, pushOutboundMessage, handleEnter } = this;
-    const { slack: { channel, slack, emoji, username, users, channels }, messages, outboundMessage, connectionState, connectionChangeTime } = this.state;
+    const { handleChange, pushOutboundMessage, handleEnter, viewUnreadMessages } = this;
+    const { unread, slack: { channel, slack, emoji, username, users, channels }, messages, outboundMessage, connectionState, connectionChangeTime } = this.state;
     return (
       <div>
         <div style={{ position: 'sticky', left: '0', top: '0', right: '0', zIndex: 1, background: '#fff' }} className="container">
@@ -145,6 +195,16 @@ export default class Room extends Component {
               <span id="header-username">@{username}</span>{' in '}
               <span id="header-channel">#{channel}</span>
             </h5>
+            {unread &&
+              <span
+                onClick={viewUnreadMessages}
+                style={{ cursor: 'pointer', width: '100%' }}
+                className="badge badge-pill badge-primary"
+              >
+                {unread.count} New Messages Since {unread.since.format()}
+                <FontAwesome style={{ marginLeft: '20px' }} name="times-circle-o" />
+              </span>
+            }
           </div>
         </div>
         <div style={{ paddingBottom: '40px', paddingTop: '40px' }} className="container">
